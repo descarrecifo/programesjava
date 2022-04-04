f1b771d478a5be1ab8fb9fee197a2d413654bd7a
LT2BS25839\Alumne_mati1 <pp@example.com>
2022-04-04 12:24:31 +0200

    commit #1 Create project

---
+++ IdeaProjects/Example_repository/src/com/company/Test.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String cognom;
+        //I dont know the value of the fields of this person david
+        // Person david = new Person();
+
+        // Person alex = new Person("Alex", "Lopez", 5, true);
+
+        // david.setAge(22);
+
+        // System.out.println("Person: " + david);
+
+        // System.out.println("Person: " + alex);
+
+        //alex.sayHello();
+
+        //alex.printName();
+
+        //cognom = alex.getSurname();
+
+        //System.out.println(cognom);
+
+        //System.out.println("Alex age to drive: " + alex.idAdult());
+
+        //System.out.println("David age to drive: " + david.idAdult());
+
+        //david.printName();
+
+        hello();
+
+        hello();
+
+        //System.out.println(alex.getSurname());
+
+        //david.setName("David");
+        //david.setSurname ("Webb");
+        //david.setAge(25);
+        //david.setVegan(true);
+
+    }
+
+    public static void hello(){
+
+        System.out.println("Hello I am a static method");
+    }
+}
+
